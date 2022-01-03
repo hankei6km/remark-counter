5b7e9498c1c4267a4f16654a6d7e8f7179143404
hankei6km <gh-hankei6km@outlook.jp>
2022-01-03 19:58:04 +0900

    Imaplement basic functions

--- test/lib/counter.spec.ts
+++ test/lib/counter.spec.ts
@@ -4,20 +4,21 @@ import remarkParse from 'remark-parse'
 import remarkStringify from 'remark-stringify'
 import remarkDirective from 'remark-directive'
 import {
-  DefineCounter,
+  SimpleCounter,
   remarkCounter,
-  RemarkNumbersOptions
-} from '../../src/lib/counter'
+  RemarkCounterOptions,
+  Counter
+} from '../../src/lib/counter.js'
 
 describe('DefineCounter', () => {
   it('should count up counter', async () => {
-    const counter = new DefineCounter()
+    const counter = new SimpleCounter()
     expect(counter.up()).toEqual(1)
     expect(counter.up()).toEqual(2)
     expect(counter.look()).toEqual(2)
   })
   it('should reset counter', async () => {
-    const counter = new DefineCounter()
+    const counter = new SimpleCounter()
     counter.addResetTrigger({ type: 'heading', depth: 2 })
     expect(counter.up()).toEqual(1)
     expect(counter.reset({ type: 'heading', depth: 2 } as Node)).toBeTruthy()
@@ -27,10 +28,52 @@ describe('DefineCounter', () => {
   })
 })
 
+describe('Counter', () => {
+  it('should define variables', async () => {
+    const numbers = new Counter()
+    numbers.define('foo')
+    numbers.define('bar')
+    expect(numbers.look('foo')).toEqual(0)
+    expect(numbers.look('bar')).toEqual(0)
+  })
+  it('should count up variables', async () => {
+    const numbers = new Counter()
+    numbers.define('foo')
+    numbers.define('bar')
+    numbers.up('foo')
+    expect(numbers.look('foo')).toEqual(1)
+    expect(numbers.look('bar')).toEqual(0)
+    numbers.up('bar')
+    expect(numbers.look('foo')).toEqual(1)
+    expect(numbers.look('bar')).toEqual(1)
+  })
+  it('should reset counter', async () => {
+    const numbers = new Counter()
+    numbers.define('foo', { type: 'heading', depth: 2 })
+    numbers.define('bar', { type: 'heading', depth: 3 })
+    expect(numbers.look('foo')).toEqual(0)
+    expect(numbers.look('bar')).toEqual(0)
+    numbers.up('foo')
+    numbers.up('bar')
+    expect(numbers.look('foo')).toEqual(1)
+    expect(numbers.look('bar')).toEqual(1)
+    numbers.reset({ type: 'heading', depth: 2 } as Node)
+    expect(numbers.look('foo')).toEqual(0)
+    expect(numbers.look('bar')).toEqual(1)
+    numbers.up('foo')
+    numbers.up('bar')
+    expect(numbers.look('foo')).toEqual(1)
+    expect(numbers.look('bar')).toEqual(2)
+    numbers.reset({ type: 'heading', depth: 3 } as Node)
+    expect(numbers.look('foo')).toEqual(1)
+    expect(numbers.look('bar')).toEqual(0)
+  })
+})
+
 describe('remarkNumbers()', () => {
   const f = async (
     html: string,
-    opts?: RemarkNumbersOptions | RemarkNumbersOptions[]
+    opts?: RemarkCounterOptions | RemarkCounterOptions[]
   ): Promise<string> => {
     return new Promise((resolve, reject) => {
       unified()
@@ -50,7 +93,7 @@ describe('remarkNumbers()', () => {
   it('should assign the value by "reset"', async () => {
     expect(
       await f(
-        '# test\n\ns1\n:num{name="fig" reset}\ns2\n\n## test1\n\n![fig1](/images/fig1.png)\nfig :num{name="fig" up}\n'
+        '# test\n\ns1\n:cnt{name="fig" reset}\ns2\n\n## test1\n\n![fig1](/images/fig1.png)\nfig :cnt{name="fig" up}\n'
       )
     ).toEqual(
       '# test\n\ns1\n\ns2\n\n## test1\n\n![fig1](/images/fig1.png)\nfig 1\n'
@@ -59,74 +102,44 @@ describe('remarkNumbers()', () => {
   it('should assign the value by "reset"(multiple)', async () => {
     expect(
       await f(
-        '# test\n\n:num{name="foo" reset}\n:num{name="bar" reset}\n\n:num{name="foo" up}\n\n:num{name="bar" up}\n\n:num{name="bar" up}\n\n:num{name="bar" up}\n\n:num{name="foo" up}\n'
+        '# test\n\n:cnt{name="foo" reset}\n:cnt{name="bar" reset}\n\n:cnt{name="foo" up}\n\n:cnt{name="bar" up}\n\n:cnt{name="bar" up}\n\n:cnt{name="bar" up}\n\n:cnt{name="foo" up}\n'
       )
     ).toEqual('# test\n\n\n\n\n1\n\n1\n\n2\n\n3\n\n2\n')
-  })
-  it('should assign the value by "define"', async () => {
-    expect(
-      await f(
-        '# test\n\ns1\n\n![fig1](/images/fig1.png)\n*fig :num{name="fig" define}*\n\ns2\n\n## test1\n\nfig :num{name="fig"}\n'
-      )
-    ).toEqual(
-      '# test\n\ns1\n\n![fig1](/images/fig1.png)\n*fig 1*\n\ns2\n\n## test1\n\nfig 1\n'
-    )
   })
   it('should increment the value by "up"', async () => {
     expect(
       await f(
-        '# test\n\n:num{name="fig" reset}\n\n:num{name="fig" up}\n\n:num{name="fig" up}\n'
+        '# test\n\n:cnt{name="fig" reset}\n\n:cnt{name="fig" up}\n\n:cnt{name="fig" up}\n'
       )
     ).toEqual('# test\n\n\n\n1\n\n2\n')
-  })
-  it('should increment the value by "define"', async () => {
-    expect(
-      await f(
-        '# test\n\n:num{name="foo" define}\n\n:num{name="bar" define}\n\n:num{name="bar" define}\n'
-      )
-    ).toEqual('# test\n\n1\n\n2\n\n3\n')
   })
   it('should reset by reset container', async () => {
     expect(
       await f(
-        '# test\n\n:::num{reset}\n## :num\n:::\n\n## head2-1\n\n:num{name="foo" define}\n\n:num{name="bar" define}\n\n## head2-2\n\n:num{name="car" define}\n\n## head2-3\n\n:num{name="foo"}:num{name="bar"}:num{name="car"}\n'
+        '# test\n\n:::cnt{reset}\n## :cnt{name="foo"}\n## :cnt{name="bar"}\n### :cnt{name="bar"}\n:::\n\n## head2-1\n\n:cnt{name="foo" up}:cnt{name="bar" up}\n\n### head3-1\n\n:cnt{name="foo" up}:cnt{name="bar" up}\n\n## head2-2\n\n:cnt{name="foo" up}:cnt{name="bar" up}\n'
       )
     ).toEqual(
-      '# test\n\n## head2-1\n\n1\n\n2\n\n## head2-2\n\n1\n\n## head2-3\n\n121\n'
+      '# test\n\n## head2-1\n\n11\n\n### head3-1\n\n21\n\n## head2-2\n\n11\n'
     )
   })
-  it('should lookup variable that is counter', async () => {
+  it('should lookup variable', async () => {
     expect(
       await f(
-        '# test\n\n:num{name="foo" reset}\n\n:num{name="foo" up}\n\n:num{name="foo" look}\n\n:num{name="foo" up}\n'
+        '# test\n\n:cnt{name="foo" reset}\n\n:cnt{name="foo" up}\n\n:cnt{name="foo" look}:cnt{name="foo"}\n\n:cnt{name="foo" up}\n'
       )
-    ).toEqual('# test\n\n\n\n1\n\n1\n\n2\n')
-  })
-  it('should lookup variable that is define at post', async () => {
-    expect(
-      await f(
-        '# test\n\n:num{name="foo" define}\n\n:nun{name="car"}\n\n:num{name="bar" define}\n\n:num{name="car" define}\n'
-      )
-    ).toEqual('# test\n\n1\n\n3\n\n2\n\n3\n')
+    ).toEqual('# test\n\n\n\n1\n\n11\n\n2\n')
   })
   it('should insert a error message if the value is not defined', async () => {
     expect(
       await f(
-        '# test\n\n:num{name="foo" reset}\n\ns1:num{name="bar" up}s2\n\ns3:num{name="car" look}s4\n\n:num{name="foo" up}\n'
+        '# test\n\n:cnt{name="foo" reset}\n\ns1:cnt{name="bar" up}s2\n\ns3:cnt{name="car" look}s4\n\ns5:cnt{name="baz"}s6\n\n:cnt{name="foo" up}\n'
       )
     ).toEqual(
-      '# test\n\n\n\ns1(ReferenceError: "bar" is not defined)s2\n\ns3(ReferenceError: "car" is not defined)s4\n\n1\n'
-    )
-    expect(
-      await f(
-        '# test\n\n:num{name="foo" define}\n\ns1:num{name="bar"}s2\n\n:num{name="foo"}\n'
-      )
-    ).toEqual(
-      '# test\n\n1\n\ns1(ReferenceError: "bar" is not defined)s2\n\n1\n'
+      '# test\n\n\n\ns1(ReferenceError: "bar" is not defined)s2\n\ns3(ReferenceError: "car" is not defined)s4\n\ns5(ReferenceError: "baz" is not defined)s6\n\n1\n'
     )
   })
   it('should escape varble name in error message', async () => {
-    expect(await f('# test\n\ns1:num{name="[bar]"}s2\n')).toEqual(
+    expect(await f('# test\n\ns1:cnt{name="[bar]"}s2\n')).toEqual(
       '# test\n\ns1(ReferenceError: "\\[bar]" is not defined)s2\n'
     )
   })
